6c0b749b659eb2a47b14786b3665f01ae13dab74
Håkan Aitch Adenbäck <ha.adenback@gmail.com>
2020-10-27 16:15:35 +0100

    Add project files.

---
+++ ADOÖvningar/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADOÖvningar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void searchBar_GotFocus(object sender, RoutedEventArgs e)
+        {
+            searchBar.Text = "";
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void categories_GotFocus(object sender, RoutedEventArgs e)
+        {
+            categories.Text = "";
+        }
+    }
+}
